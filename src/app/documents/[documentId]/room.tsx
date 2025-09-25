b50e3b57c8d51c09d4071c50ae631b05d0720737
Tithaye Beri <tithaye.beri@gmail.com>
2025-09-25 23:10:26 +0530

    fix imports

---
+++ src/app/documents/[documentId]/room.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { ReactNode } from "react";
+import {
+  LiveblocksProvider,
+  RoomProvider,
+  ClientSideSuspense,
+} from "@liveblocks/react/suspense";
+import { useParams } from "next/navigation";
+
+export function Room({ children }: { children: ReactNode }) {
+    const params = useParams();
+  return (
+    <LiveblocksProvider publicApiKey={"pk_dev_41eMlO7gu0OmVV67rVDb49YP1dDVDi5ZxUNuX-84eJss2xqF9ycMAbJDPIOaGA4A"}>
+      <RoomProvider id={params.documentId as string}>
+        <ClientSideSuspense fallback={<div>Loading…</div>}>
+          {children}
+        </ClientSideSuspense>
+      </RoomProvider>
+    </LiveblocksProvider>
+  );
+}
