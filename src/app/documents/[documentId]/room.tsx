d7efc0811954909d3ba2927aa9440802f7a36600
Tithaye Beri <tithaye.beri@gmail.com>
2025-09-25 23:47:01 +0530

    remove liveblocks

--- src/app/documents/[documentId]/room.tsx
+++ src/app/documents/[documentId]/room.tsx
@@ -1,4 +1,4 @@
-"use client";
+/*"use client";
 
 import { ReactNode } from "react";
 import {
@@ -19,4 +19,4 @@ export function Room({ children }: { children: ReactNode }) {
       </RoomProvider>
     </LiveblocksProvider>
   );
-}
+}*/
